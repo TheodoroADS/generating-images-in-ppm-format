3f66b0a241645095d5f214c6083f0582a29e7eb8
Theodoro MARTIN AHUALLI <theodoro.ahualli-de-sanctis@insa-rennes.fr>
2022-07-14 00:18:36 -0300

    OUVIRAM DO IPIRANGA

--- ppmfun.py
+++ ppmfun.py
@@ -6,6 +6,9 @@ HEIGHT = 256
 cx = int(WIDTH/2)
 cy = int(HEIGHT/2)
 
+sqrt2over2 = 0.7071
+
+
 def frag(u,v):
     u = u/WIDTH
     v = v/HEIGHT
@@ -17,13 +20,13 @@ def grid(u,v): #interesting: I made a mistake and let the values of rgb be <0, b
 
 
 def color_oscillation(u,v):
-    return ( (sin(u) + 1)/2 , (cos(v) +1)/2, 0 )
+    return ( (cos(u) + 1)/2 , (sin(v) +1)/2, 100 )
 
 def crazy(u,v):
     u = u/WIDTH
     v = v/HEIGHT
 
-    return (u+v, u-v, u)
+    return (u+v, u-v, 255)
 
 def xadrez(u,v):
 
@@ -49,6 +52,46 @@ def circle(u,v, radius):
     else:
         return (1,1,1)   
 
+def france(u):
+
+    if(u < WIDTH/3):
+        return (0,0,1) #blue
+    elif (u < 2* WIDTH/3):
+        return (1,1,1) #white
+    else:
+        return (255,0,0) #red
+
+def brazil(u,v,a,b, radius, ribbon_width):
+
+
+    # checking if the coordinates are inside the diamond 
+    #change of basis 
+    uu = (u - cx +v - cy)*sqrt2over2
+    vv = (v - cy -u + cx) * sqrt2over2
+     
+    if uu > -a/2  and uu < a/2  and vv > - b/2 and vv < b/2 :       
+
+        #checking if we are inside the circle
+
+        if ((u - cx)**2 + (v - cy)**2 <= radius):
+
+            if (uu < ribbon_width/2 and uu > -ribbon_width/2):
+                return (0.9,0.9,0.9)
+            else:
+                return (0, 0 , 0.8) #circle color
+        else:
+            return (0.7,0.7, 0) #diamond color
+    else:
+        return (0,0.2,0) #flag background
+
+
+def centerd_square(u,v):
+
+    if u - cx < 100 and u - cx > -100 and v - cy < 100 and v - cy > -100:
+        return (1,0,0)
+    else:
+        return (0,0,1)   
+
 
 f = open("img.ppm", "w")
 
@@ -57,7 +100,8 @@ f.write(f"P3 \n {WIDTH} {HEIGHT} 255 \n")
 for y in range(HEIGHT):
     for x in range(WIDTH):
 
-        r , g, b = crazy(x,y)
+        r , g, b = brazil(x,y, 150, 150, 1500, 10)
+        # r , g, b = centerd_square(x,y)c
 
         f.write(f"{int(r*255)} {int(g*255)} {int(b*255)} ")
 
